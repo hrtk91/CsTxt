7adab041e40677e7f2c92b7b3977818e74db4413
安富 孝司 <koji_micro@hotmail.co.jp>
2018-12-24 11:58:56 +0900

    テストプロジェクト作成

---
+++ CSTPad.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSTPad.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("CSTPad.Test")]
+[assembly: AssemblyCopyright("Copyright © Company 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df3561ed-35d4-44d1-9527-f994475aae68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSTPad.Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Data;
+using CSTPad.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSTPad.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IValueConverter converter = new CSharpResultConverter();
+
+            var result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IValueConverter converter = new CSharpResultConverter();
+
+            var result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+            result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+            result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+            result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+            result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+            result = converter.Convert("@using System;\r\n@{= 20 }", null, null, null);
+        }
+    }
+}
