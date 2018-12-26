63edfebf1d48cc8bdd0f55c65cac7f1c007f0c93
安富 孝司 <koji_micro@hotmail.co.jp>
2018-12-26 22:18:50 +0900

    複数行インデント対応
    ・ 複数行選択状態&TAB押下で選択範囲インデント
    ・ 複数行選択状態&SHIFT&TAB押下で選択範囲インデント解除

--- CSTPad/Model/Text/TabToIndentProcessor.cs
+++ CSTPad/Model/Text/TabToIndentProcessor.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -19,24 +21,26 @@ namespace CSTPad.Model.Text
                 {
                     if (0 == AssociatedObject.SelectionLength)
                     {
-                        // 単行インデント追加
-                        InsertOneLineTab(caret);
+                        // 単行インデント削除
+                        RemoveOneLineTab(text, caret);
                     }
                     else
                     {
-                        // TODO: 複数行インデント追加
+                        // 複数行インデント削除
+                        RemoveMultiLineTab(text, caret);
                     }
                 }
                 else
                 {
                     if (0 == AssociatedObject.SelectionLength)
                     {
-                        // 単行インデント削除
-                        RemoveOneLineTab(caret);
+                        // 単行インデント追加
+                        InsertOneLineTab(text, caret);
                     }
                     else
                     {
-                        // TODO: 複数行インデント削除
+                        // 複数行インデント追加
+                        InsertMultiLineTab(text, caret);
                     }
                 }
 
@@ -44,19 +48,27 @@ namespace CSTPad.Model.Text
             }
         }
 
-        private void InsertOneLineTab(int caret)
+        private void InsertOneLineTab(string text, int caret)
+        {
+            // Tab: 1段階インデント追加
+            AssociatedObject.Text = text.Insert(caret, "    ");
+
+            AssociatedObject.CaretIndex = caret + INDENT.Length;
+        }
+
+        private void RemoveOneLineTab(string text, int caret)
         {
             // Shift + Tab: 1段階インデント解除
             StringBuilder sb = new StringBuilder();
 
             for (int i = caret - 1; 0 < i; i--)
             {
-                if ('\n' == AssociatedObject.Text[i])
+                if ('\n' == text[i])
                 {
                     break;
                 }
 
-                sb.Insert(0, AssociatedObject.Text[i]);
+                sb.Insert(0, text[i]);
             }
 
             string line = sb.ToString();
@@ -65,24 +77,47 @@ namespace CSTPad.Model.Text
             {
                 if (INDENT.Length <= line.Length)
                 {
-                    AssociatedObject.Text = AssociatedObject.Text.Remove(caret - INDENT.Length, INDENT.Length);
+                    AssociatedObject.Text = text.Remove(caret - INDENT.Length, INDENT.Length);
                     AssociatedObject.CaretIndex = caret - INDENT.Length;
                 }
                 else
                 {
-                    AssociatedObject.Text = AssociatedObject.Text.Remove(caret - line.Length, line.Length);
+                    AssociatedObject.Text = text.Remove(caret - line.Length, line.Length);
                     AssociatedObject.CaretIndex = caret - line.Length;
                 }
             }
         }
 
-        private void RemoveOneLineTab(int caret)
+        private void InsertMultiLineTab(string text, int caret)
         {
-            // Tab: 1段階インデント追加
-            AssociatedObject.Text =
-                AssociatedObject.Text.Insert(caret, "    ");
+            int selectionStart = AssociatedObject.SelectionStart;
+            int selectionEnd = selectionStart + AssociatedObject.SelectionLength;
+            string selectionText = AssociatedObject.Text.Substring(selectionStart, AssociatedObject.SelectionLength);
+            string newText = Regex.Replace(selectionText, "^", INDENT, RegexOptions.Multiline);
 
-            AssociatedObject.CaretIndex = caret + INDENT.Length;
+            AssociatedObject.Text =
+                text.Substring(0, selectionStart) +
+                newText +
+                text.Substring(selectionEnd);
+
+            AssociatedObject.SelectionStart = selectionStart;
+            AssociatedObject.SelectionLength = newText.Length;
+        }
+
+        private void RemoveMultiLineTab(string text, int caret)
+        {
+            int selectionStart = AssociatedObject.SelectionStart;
+            int selectionEnd = selectionStart + AssociatedObject.SelectionLength;
+            string selectionText = AssociatedObject.Text.Substring(selectionStart, AssociatedObject.SelectionLength);
+            string newText = Regex.Replace(selectionText, "^ {0,4}", string.Empty, RegexOptions.Multiline);
+
+            AssociatedObject.Text =
+                text.Substring(0, selectionStart) +
+                newText +
+                text.Substring(selectionEnd);
+
+            AssociatedObject.SelectionStart = selectionStart;
+            AssociatedObject.SelectionLength = newText.Length;
         }
     }
 }
